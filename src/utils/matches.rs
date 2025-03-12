1186e61863a92a1401105acba2df7e186000d15e
cnavajas <c.navajas4@gmail.com>
2025-03-12 17:21:02 +0100

    minimal setup

--- src/utils/matches.rs
+++
@@ -1,5 +0,0 @@
-use wildmatch::WildMatch;
-
-pub fn matches(pattern: &str, domain: &str) -> bool {
-    WildMatch::new(pattern).matches(domain)
-}
--- src/utils/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod atomic_ref_counter;
-pub mod matches;
-pub mod stream_id;
-
-pub use self::matches::matches;
